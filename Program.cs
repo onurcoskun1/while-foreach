4e03ec3fc14505048ab83d118f7607368dc42c7c
onurcoskun1 <coskunonur01@gmail.com>
2022-02-11 19:12:59 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //while
+            //1 den başlayarak konsoldan girilen sayı dahil ortalama hesaplayıp konsola yazdıralım.
+           //Console.Write("Lütfen bir sayı giriniz:");
+           //int sayi = int.Parse(Console.ReadLine());
+           //int sayac=1;
+           //int toplam=0;
+           //while (sayac<=sayi) 
+           //{
+           //    toplam+=sayac;
+           //    sayac ++;
+           //}
+           //Console.WriteLine(toplam/sayi);
+           //
+           //// a'dan z'ye kadar bütün harfleri console'a yazdır.
+           //
+           //char karakter = 'a';
+           //
+           //while (karakter<'z')
+           //{
+           //    Console.Write(karakter);
+           //    karakter++;
+           //}
+            
+            string[] arabalar = {"Bmw", "Ford", "Toyota", "Lambo"};
+            
+            foreach (var araba in arabalar)
+	        {
+                Console.WriteLine(araba);
+	        }
+        }
+    }
+}
